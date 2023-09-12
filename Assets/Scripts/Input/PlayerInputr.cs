7e5302ef7ec4550cebe05a90892110aee8d03484
Rstar01 <4A9k0001@stust.edu.tw>
2023-09-12 23:45:55 +0800

    workday 9/12

--- Assets/Scripts/Input/PlayerInputr.cs
+++ Assets/Scripts/Input/PlayerInputr.cs
@@ -2,19 +2,54 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UniRx;
+using System;
+using Unity.VisualScripting;
 
 public class PlayerInputr : MonoBehaviour
 {
-    void OnJump(InputValue value)
+    public static PlayerInputr Instance { get; private set; }
+    private void Awake()
     {
-        Debug.Log("Jump"+ value.isPressed.ToString());
-        
+        // If there is an instance, and it's not me, delete myself.
+
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+        }
     }
 
-    void jumpUP()
+    //ª±®aInput
+    public InputActionAsset inputActionAsset;
+    private InputActionMap playerAct;
+
+    private ReactiveCollection<InputAction> Actions = new ReactiveCollection<InputAction>();
+    //public IObservable<InputAction> OnInputActionChanged => Actions;
+
+    private void Start()
     {
+        inputActionAsset = GetComponent<PlayerInput>().actions;
+        playerAct = inputActionAsset.FindActionMap("Player");
+
+        //Actions.Subscribe(x => Debug.Log(x));
+
+        Actions.ObserveAdd()
+            .Subscribe(x => Debug.Log(x));
+
+        Actions.Add(playerAct.FindAction("Jump"));
+        Actions.Add(playerAct.FindAction("Movement"));
+
+        Actions.ObserveEveryValueChanged(x => x)
+            .Subscribe(_ =>
+            {
+                print("Pressed");
+            });
+
 
     }
 
-    void jumpDOWN() { }
 }
--- Assets/Scripts/MultTrigger/UniRxTest.cs
+++ Assets/Scripts/MultTrigger/UniRxTest.cs
@@ -7,21 +7,27 @@ using System;
 
 public class UniRxTest : MonoBehaviour
 {
-    //ª±®aInput
-    public InputActionAsset inputActionAsset;
-    private InputActionMap playerAct;
-
-    // Start is called before the first frame update
     void Start()
     {
-        inputActionAsset = GetComponent<PlayerInput>().actions;
-        playerAct = inputActionAsset.FindActionMap("Player");
+        //PlayerInputr.Instance.OnInputActionChanged
+        //    .Where(x => x.WasPressedThisFrame())
+        //    .Subscribe(_ =>
+        //    {
+        //        print("Pressed");
+        //    });
+        //
+        //PlayerInputr.Instance.OnInputActionChanged
+        //    .Where(x => x.WasReleasedThisFrame())
+        //    .Subscribe(_ =>
+        //    {
+        //        print("Released");
+        //    });
 
-        var doubleClick = Observable.EveryUpdate().Where(value => playerAct.FindAction("Jump").WasPressedThisFrame());
-
-        doubleClick.Buffer(doubleClick.Throttle(TimeSpan.FromMilliseconds(250)))
-            .Where(value => value.Count >= 2)
-            .Subscribe(value => Debug.Log("doubleClick:" + value.Count));
+        //var doubleClick = Observable.EveryUpdate().Where(value => playerAct.FindAction("Jump").WasPressedThisFrame());
+        //
+        //doubleClick.Buffer(doubleClick.Throttle(TimeSpan.FromMilliseconds(250)))
+        //    .Where(value => value.Count >= 2)
+        //    .Subscribe(value => Debug.Log("doubleClick:" + value.Count));
     }
 
 }
