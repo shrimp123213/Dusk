4575234f9d0de0564ca734a4b59b8ec77175a8a9
shrimp123213 <shrimp123213@gmail.com>
2023-10-04 00:22:29 +0800

    行為樹測試_追蹤目標
    追蹤玩家

--- Assets/Actions/MoveToPosition2D.cs
+++ Assets/Actions/MoveToPosition2D.cs
@@ -9,37 +9,33 @@ public class MoveToPosition2D : ActionNode
     [Tooltip("How fast to move")]
     public NodeProperty<float> speed = new NodeProperty<float> { defaultValue = 5.0f };
 
-    [Tooltip("Stop within this distance of the target")]
-    public NodeProperty<float> stoppingDistance = new NodeProperty<float> { defaultValue = 0.1f };
-
-    [Tooltip("Updates the agents rotation along the path")]
-    public NodeProperty<bool> updateRotation = new NodeProperty<bool> { defaultValue = true };
-
-    [Tooltip("Maximum acceleration when following the path")]
-    public NodeProperty<float> acceleration = new NodeProperty<float> { defaultValue = 40.0f };
-
-    [Tooltip("Returns success when the remaining distance is less than this amount")]
-    public NodeProperty<float> tolerance = new NodeProperty<float> { defaultValue = 1.0f };
-
-    [Tooltip("Target Position")]
-    public NodeProperty<Vector2> targetPosition = new NodeProperty<Vector2> { defaultValue = Vector2.zero };
-    
-    [Tooltip("Move Time")]
-    public NodeProperty<float> moveTime = new NodeProperty<float> { defaultValue = 3.0f };
+    [Tooltip("Target")]
+    public NodeProperty<GameObject> target = new NodeProperty<GameObject> { defaultValue = null };
     
     
     protected override void OnStart() {
         if (context.gameObject != null)
         {
-            Rigidbody2D contextRb = context.gameObject.GetComponent<Rigidbody2D>();
-            //contextRb.velocity = new Vector2(speed.Value,0);
-            if(contextRb.transform.position.x < targetPosition.Value.x)
+            //Rigidbody2D contextRb = context.gameObject.GetComponent<Rigidbody2D>();
+            Transform targetTransform = target.Value.transform;
+
+            if (target!=null)
             {
-                contextRb.velocity = new Vector2(speed.Value,0);
-            }
-            else
-            {
-                contextRb.velocity = new Vector2(-speed.Value,0);
+                context.transform.position= Vector2.MoveTowards(context.transform.position, targetTransform.position, speed.Value * Time.deltaTime);
+                if(context.transform.position.x >= targetTransform.position.x)
+                {
+                    context.transform.GetChild(0).localScale = new Vector3(-5, 5, 5);
+                    /*new Vector3(context.transform.GetChild(0).localScale.x*-1f,
+                        context.transform.GetChild(0).localScale.y,
+                        context.transform.GetChild(0).localScale.z);*/
+                }
+                else
+                {
+                    context.transform.GetChild(0).localScale = new Vector3(5, 5, 5);
+                    /*new Vector3(context.transform.GetChild(0).localScale.x*1f,
+                        context.transform.GetChild(0).localScale.y,
+                        context.transform.GetChild(0).localScale.z);*/
+                }
             }
         }
     }
