635449938f1a6a79f20df326c699ac089106d3b5
DESKTOP-6GB6O5C\林辛源 <4a9k0001@stust.edu.tw>
2024-01-03 02:15:05 +0800

    主教開場演出更新；字體更新

--- Assets/Scripts/Scene/Contact.cs
+++ Assets/Scripts/Scene/Contact.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using BehaviorDesigner.Runtime;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
+using DG.Tweening;
 
 public class Contact : MonoBehaviour
 {
@@ -14,10 +16,21 @@ public class Contact : MonoBehaviour
 
     public Transform BossHealthBar;
     public TMP_Text BossName;
-    
+    public Image BossNameBackground;
+
     private float speed;
     private float waitTime;
     private float waitTimeMax = .5f;
+
+    public Vector2 endPos;
+    public Vector2 endSize;
+    private int startFontSize;
+    public int endFontSize;
+    private float progress;
+    private bool startedMove;
+
+    private bool triggered;
+    private bool aniStoped;
 
     void Start()
     {
@@ -31,56 +44,96 @@ public class Contact : MonoBehaviour
     {
         if (ani.GetCurrentAnimatorClipInfo(0).Length > 0 && ani.GetCurrentAnimatorClipInfo(0)[0].clip.name == "boss1-1_ST_start") 
         {
-            if (ani.GetCurrentAnimatorStateInfo(0).normalizedTime > 285f / 501f) 
+            if (ani.GetCurrentAnimatorStateInfo(0).normalizedTime > 285f / 501f && ani.GetCurrentAnimatorStateInfo(0).normalizedTime < 435f / 501f) 
             {
                 fences[0].Play("FenceUp");
                 fences[1].Play("FenceUp");
-
-                speed = 1f;
-
             }
-            if (ani.GetCurrentAnimatorStateInfo(0).normalizedTime > 330f / 501f)
+            else if (ani.GetCurrentAnimatorStateInfo(0).normalizedTime > 435f / 501f && ani.GetCurrentAnimatorStateInfo(0).normalizedTime < 450f / 501f)
             {
-                //AITree.GetComponent<Character>().HitEffect.HitStun = .01f;
-
-                Camcam.i.UseOverride = false;
-
-                PlayerMain.i.CanInput = true;
+                speed = 1f;
             }
-;
+            else if (ani.GetCurrentAnimatorStateInfo(0).normalizedTime > 450f / 501f && ani.GetCurrentAnimatorStateInfo(0).normalizedTime < 495f / 501f && !aniStoped)
+            {
+                aniStoped = true;
+                ani.speed = 0;
+            }
+            else if (ani.GetCurrentAnimatorStateInfo(0).normalizedTime > 495f / 501f)
+            {
+                ani.speed = 0;
+            }
         }
-        
+        if (ani.GetCurrentAnimatorClipInfo(0).Length > 0 && ani.GetCurrentAnimatorClipInfo(0)[0].clip.name != "boss1-1_ST_start" && ani.GetCurrentAnimatorClipInfo(0)[0].clip.name != "boss1-1_ST_sit_idle") 
+        {
+            //AITree.GetComponent<Character>().HitEffect.HitStun = .01f;
+
+            Camcam.i.UseOverride = false;
+
+            PlayerMain.i.CanInput = true;
+        }
+
         if (speed > 0f)
         {
             if (waitTime > 0f)
                 waitTime -= Time.deltaTime;
             else
             {
-                if (BossName.text == "")
+                if (BossNameBackground.rectTransform.rect.size.x <= 3000)
                 {
-                    waitTime = waitTimeMax;
-                    BossName.text = "主";
+                    BossNameBackground.color = BossNameBackground.color + new Color(0f, 0f, 0f, 1.5f * Time.deltaTime);
+                    BossNameBackground.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, BossNameBackground.rectTransform.rect.size.x + 6000f * Time.deltaTime);
+                    BossNameBackground.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, BossNameBackground.rectTransform.rect.size.y + 6000f * Time.deltaTime);
                 }
-                else if (BossName.text == "主")
+                else
                 {
-                    waitTime = waitTimeMax;
-                    BossName.text = "主教";
+                    if (BossName.text == "")
+                    {
+                        waitTime = waitTimeMax;
+                        BossName.text = "主";
+                    }
+                    else if (BossName.text == "主")
+                    {
+                        waitTime = waitTimeMax * 2;
+                        BossName.text = "主教";
+                    }
                 }
             }
 
             if (BossName.text == "主教" && waitTime <= 0f) 
             {
-                BossHealthBar.localScale = new Vector3(Mathf.MoveTowards(BossHealthBar.localScale.x, .75f, speed * Time.deltaTime), .75f, .75f);
+                if (!startedMove)
+                {
+                    startedMove = true;
+                    BossName.rectTransform.DOLocalMove(endPos, speed - .1f);
+                    BossName.rectTransform.DOSizeDelta(endSize, speed - .1f);
+
+                    startFontSize = (int)BossName.fontSize;
+                    progress = 0;
+                    ani.speed = 1;
+                }
+                else
+                {
+                    progress += speed * 1.25f * Time.deltaTime; 
+                    BossName.fontSize = Mathf.Lerp(startFontSize, endFontSize, progress);
+                    BossNameBackground.color = BossNameBackground.color - new Color(0f, 0f, 0f, .25f * progress);
+                }
+
+                if (BossName.rectTransform.localPosition == (Vector3)endPos)
+                {
+                    ani.speed = 1;
+                    BossHealthBar.localScale = new Vector3(Mathf.MoveTowards(BossHealthBar.localScale.x, .75f, speed * Time.deltaTime), .75f, .75f);
+                }
 
                 if (BossHealthBar.localScale.x == .75f)
                     gameObject.SetActive(false);
             }
         }
+        Debug.Log(ani.speed);
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && !triggered)
         {
             AITree.enabled = true;
             //ani.Play("boss1-1_ST_start");
@@ -93,6 +146,8 @@ public class Contact : MonoBehaviour
             PlayerMain.i.CanInput = false;
             
             Camcam.i.Boss = AITree.transform;
+
+            triggered = true;
         }
     }
 }
