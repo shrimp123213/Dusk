cfcf1f2fbd84080294a3417cd27762694b763c35
shrimp123213 <shrimp123213@gmail.com>
2024-01-16 17:02:40 +0800

    增加動作冷卻判斷

--- Assets/Behaviour Tree/Conditional/ActionCoolDown.cs
+++ Assets/Behaviour Tree/Conditional/ActionCoolDown.cs
@@ -8,34 +8,58 @@ public class ActionCoolDown : EnemyConditionalBase
 {
     public string CoolDownActionName;
     public float CoolDownTime;
+
+    public BehaviorTree behaviorTree;
+    private Task task;
     
-    // The time to wait
-    private float waitDuration;
     // The time that the task started to wait.
-    private float startTime;
+    private float startTime = -1;
     // Remember the time that the task is paused so the time paused doesn't contribute to the wait time.
     private float pauseTime;
+    private bool CDing = false;
 
-    public override void OnStart()
+    public override void OnAwake()
     {
-        startTime = Time.time;
-        waitDuration = CoolDownTime;
+        behaviorTree = GetComponent<BehaviorTree>();
+        task = behaviorTree.FindTaskWithName(CoolDownActionName);
+        /*if (task != null)
+        {
+            if (task.OnUpdate() == TaskStatus.Success || task.OnUpdate() == TaskStatus.Failure)
+            {
+                startTime = Time.time;
+            }
+        }*/
     }
 
     public override TaskStatus OnUpdate()
     {
-        if (this.SelfCharacter.Value.NowAction.Id == CoolDownActionName)
+        CDing = startTime + CoolDownTime > Time.time;
+        Debug.Log("CDing:"+CDing);
+        if(!CDing)
         {
-            if (startTime + waitDuration < Time.time) {
-                return TaskStatus.Success;
-            }
-            return TaskStatus.Failure;
-            // Otherwise we are still waiting.
+            startTime = task.OnUpdate() == TaskStatus.Success ? Time.time : 0;
         }
-        return TaskStatus.Running;
+
+        Debug.Log("CD:" + ((startTime + CoolDownTime) - Time.time));
+        
+        if(CDing)
+        {
+            return TaskStatus.Failure;
+        }
+        
+        return TaskStatus.Success;
+
+        // Otherwise we are still waiting.
+        //return TaskStatus.Running;
     }
-    
-    public override void OnPause(bool paused)
+
+    public override void OnReset()
+    {
+        //startTime = 0;
+        //CDing = false;
+    }
+
+    /*public override void OnPause(bool paused)
     {
         if (paused) {
             // Remember the time that the behavior was paused.
@@ -44,5 +68,5 @@ public class ActionCoolDown : EnemyConditionalBase
             // Add the difference between Time.time and pauseTime to figure out a new start time.
             startTime += (Time.time - pauseTime);
         }
-    }
+    }*/
 }
